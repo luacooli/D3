1a9429a7414d00421a99fba28630e625052406e1
Luana <lcorreia@vntrx.com>
2020-05-06 16:24:10 -0300

    joing multiple elements

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const data = [
   {
     width: 200,
     height: 100,
-    fill: 'purple'
-  }
+    fill: 'purple',
+  },
+  {
+    width: 100,
+    height: 60,
+    fill: 'pink',
+  },
+  {
+    width: 50,
+    height: 30,
+    fill: 'red',
+  },
 ]
 
 const svg = d3.select('svg')
 
-const rect = svg.select('rect')
+const rect = svg
+  .selectAll('rect')
   .data(data)
-  .attr('width', function(d) { return d.width })
-  .attr('height', function(d) { return d.height })
-  .attr('fill', function(d) { return d.fill })
+  .attr('width', (d, i, n) => {
+    // console.log(n[i])
+    return d.width
+  })
+  .attr('height', (d) => d.height)
+  .attr('fill', (d) => d.fill)
