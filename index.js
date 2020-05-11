ac6c1d0357e351c6080ec76760f0ec5f0e0a336e
Luana <lcorreia@vntrx.com>
2020-05-11 08:55:20 -0300

    external data json

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
-const data = [
-  {
-    width: 200,
-    height: 100,
-    fill: 'purple',
-  },
-  {
-    width: 100,
-    height: 60,
-    fill: 'pink',
-  },
-  {
-    width: 50,
-    height: 30,
-    fill: 'red',
-  },
-]
+// select svg container first
 
 const svg = d3.select('svg')
 
-// join data to rects
-const rects = svg
-  .selectAll('rect')
-  .data(data)
+d3.json('planets.json').then((data) => {
+  const circs = svg.selectAll('circle').data(data)
 
-// add attrs to reacts already to the DOM
-rects.attr('width', (d, i, n) => {
-    // console.log(n[i])
-    return d.width
-  })
-  .attr('height', (d) => d.height)
-  .attr('fill', (d) => d.fill)
+  // add attrs to circs already in DOM
+  circs
+    .attr('cy', 200)
+    .attr('cx', (d) => d.distance)
+    .attr('r', (d) => d.radius)
+    .attr('fill', (d) => d.fill)
 
-// append the enter selection to the DOM
-rects.enter()
-  .append('rect')
-  .attr('width', (d, i, n) => d.width)
-  .attr('height', (d) => d.height)
-  .attr('fill', (d) => d.fill)
-  
+  //append the enter selection to the DOM
+  circs
+    .enter()
+    .append('circle')
+    .attr('cy', 200)
+    .attr('cx', (d) => d.distance)
+    .attr('r', (d) => d.radius)
+    .attr('fill', (d) => d.fill)
+})
