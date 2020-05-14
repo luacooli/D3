f508d770483add00a5b0f98dc6596a9c2536512d
Luana <lcorreia@vntrx.com>
2020-05-14 08:38:28 -0300

    tick formatting and firebase

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ const xAxisGroup = graph.append('g')
   .attr('transform', `translate(0, ${graphHeigth})`)
 const yAxisGroup = graph.append('g')
 
-d3.json('menu.json').then((data) => {
+db.collection('dishes').get().then((res) => {
+  console.log(res)
+  var data = []
+
+  res.docs.forEach(doc => {
+    data.push(doc.data())
+  });
+
+  console.log(data)
+
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, (d) => d.orders)])
@@ -38,23 +47,32 @@ d3.json('menu.json').then((data) => {
   // add attrs to rects already in DOM
   rects
     .attr('width', x.bandwidth)
-    .attr('height', (d) => y(d.orders))
+    .attr('height', (d) => graphHeigth - y(d.orders))
     .attr('fill', 'orange')
     .attr('x', (d) => x(d.name))
+    .attr('y', d => y(d.orders))
 
   //append enter selection to the DOM
   rects
     .enter()
     .append('rect')
     .attr('width', x.bandwidth)
-    .attr('height', (d) => y(d.orders))
+    .attr('height', (d) => graphHeigth - y(d.orders))
     .attr('fill', 'orange')
     .attr('x', (d) => x(d.name))
+    .attr('y', d => y(d.orders))
     
     // create and call the axes
     const xAxis = d3.axisBottom(x)
     const yAxis = d3.axisLeft(y)
+      .ticks(3)
+      .tickFormat(d => d + ' orders')
     
     xAxisGroup.call(xAxis)
     yAxisGroup.call(yAxis)
+
+    xAxisGroup.selectAll('text')
+      .attr('transform', 'rotate(-40)')
+      .attr('text-anchor', 'end')
+      .attr('fill', 'orange')
 })
